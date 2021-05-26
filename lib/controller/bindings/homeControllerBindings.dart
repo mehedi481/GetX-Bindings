45289fc25ae54629eaf907528ee363d66d2d558a
Mehedi <mehedihasanmax95@gmail.com>
2021-05-26 19:16:39 +0600

    Use lazyPut

--- lib/controller/bindings/homeControllerBindings.dart
+++ lib/controller/bindings/homeControllerBindings.dart
@@ -4,6 +4,7 @@ import 'package:getx_binding/controller/homeController/homeController.dart';
 class HomeBindings extends Bindings{
   @override
   void dependencies() {
+    // Note: use lazyPut if faces any problem
     Get.lazyPut(()=>HomePageController());
   }
 
